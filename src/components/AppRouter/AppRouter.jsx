74ddf0a8bcc41440a89c9c6e8df59386767f1031
ignatovich01 <nik.ignatovich2001@gmail.com>
2023-08-31 20:30:01 +0300

    add pages package

---
+++ src/components/AppRouter/AppRouter.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+// import App from '../../App';
+
+export function AppRouter() {
+   return <div>1</div>;
+}
