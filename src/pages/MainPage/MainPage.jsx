0ceca4ed8df8fd3bbe15aa57b7d8fd3e7578107b
ignatovich01 <nik.ignatovich2001@gmail.com>
2023-09-02 05:20:37 +0300

    some ui

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 
+import { Header } from '../../components/Header/Header';
+
 import { useGetMoviesQuery } from '../../store/API/moviesAPI';
 
 export function MainPage() {
    /* TODO цифра в запросе = страница, временная заглушка */
    const { data = [], isLoading, error } = useGetMoviesQuery(11);
 
-   if (isLoading) return <div>Loading...</div>;
+   // if (isLoading) return <div>Loading...</div>;
    if (error) return <div>Some Error</div>;
 
    return (
-      <ul>
-         {data.docs.map((item) => (
-            <li key={item.id}>{item.id}</li>
-         ))}
-      </ul>
+      <div>
+         <Header />
+         {isLoading ? (
+            <div>Loading...</div>
+         ) : (
+            <ul>
+               {data.docs.map((item) => (
+                  <li key={item.id}>{item.id} </li>
+               ))}
+            </ul>
+         )}
+      </div>
    );
 }
