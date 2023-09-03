2a91e96a6e12423ae2f4fa142dbd3132a7c63a11
ignatovich01 <nik.ignatovich2001@gmail.com>
2023-09-03 21:35:56 +0300

    add userSlice to store

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { favorites } from './slices/favoriteSlice';
 import { history } from './slices/historySlice';
 import { moviesApi } from './API/moviesAPI';
+import { user } from './slices/userSlice';
 
 export const store = configureStore({
    reducer: {
       [moviesApi.reducerPath]: moviesApi.reducer,
       favorites,
       history,
+      user,
    },
    middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(moviesApi.middleware),
