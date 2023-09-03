587a47f6bbb6c34e5965fb7216cd8b13dde1380c
ignatovich01 <nik.ignatovich2001@gmail.com>
2023-09-03 05:18:17 +0300

    ui

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,13 +3,18 @@ import React from 'react';
 import { Header } from '../../components/Header/Header';
 
 import { useGetMoviesQuery } from '../../store/API/moviesAPI';
+import { ItemComponent } from '../../components/ItemComponent/ItemComponent';
 
 export function MainPage() {
    /* TODO цифра в запросе = страница, временная заглушка */
    const { data = [], isLoading, error } = useGetMoviesQuery(11);
 
-   // if (isLoading) return <div>Loading...</div>;
+   if (isLoading) return <div>Loading...</div>;
    if (error) return <div>Some Error</div>;
+   //TODO просто проверял и думал как лучше реализовать
+   if (!isLoading) {
+      console.log(data.docs[1].poster);
+   }
 
    return (
       <div>
@@ -17,11 +22,19 @@ export function MainPage() {
          {isLoading ? (
             <div>Loading...</div>
          ) : (
-            <ul>
+            <div
+               style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  flexWrap: 'wrap',
+                  margin: '20px',
+                  justifyContent: 'space-around',
+               }}
+            >
                {data.docs.map((item) => (
-                  <li key={item.id}>{item.id} </li>
+                  <ItemComponent key={item.id} Id={item.id} />
                ))}
-            </ul>
+            </div>
          )}
       </div>
    );
