e2e04a2151d98367426638cb913dc9c8bba92c04
ignatovich01 <nik.ignatovich2001@gmail.com>
2023-09-04 01:55:09 +0300

    refactor and setting authMiddleware and check correct work

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,23 +1,10 @@
-import React, { useEffect } from 'react';
-
-// import { useDispatch } from 'react-redux';
+import React from 'react';
 
 import { Header } from '../../components/Header/Header';
 
 import { useGetMoviesQuery } from '../../store/API/moviesAPI';
 
-// import { setUser } from '../../store/slices/userSlice';
-
 export function MainPage() {
-   /* TODO проверка middleware */
-   // const dispatch = useDispatch();
-   // const foo = () => {
-   //    dispatch(setUser({ name: 'nikita', password: '123245' }));
-   // };
-   // useEffect(() => {
-   //    foo();
-   // }, []);
-
    /* TODO цифра в запросе = страница, временная заглушка */
    const { data = [], isLoading, error } = useGetMoviesQuery(11);
 
