a1207ef1b75787f9659bf2da60865b26ea6da063
ignatovich01 <nik.ignatovich2001@gmail.com>
2023-09-06 07:06:05 +0300

    add error boundary and use in main-page

---
+++ src/components/ErrorBoundaries/ErrorBoundaries.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+/* eslint-disable react/destructuring-assignment */
+import React, { Component } from 'react';
+
+export class ErrorBoundaries extends Component {
+   constructor() {
+      super();
+      this.state = {
+         error: false,
+      };
+   }
+
+   componentDidCatch(error, errorInfo) {
+      this.setState({
+         error: true,
+      });
+
+      console.log(`Uncaught error : ${error} ${errorInfo}`);
+   }
+
+   render() {
+      if (this.state.error === true) {
+         return <div>Предохранитель словил ошибку</div>;
+      }
+      return this.props.children;
+   }
+}
